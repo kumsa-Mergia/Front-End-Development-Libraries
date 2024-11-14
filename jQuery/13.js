1e4fb20ff97eebe4863583019b5932cb432d0fbc
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 10:39:55 +0300

    Clone an Element Using jQuery

---
+++ jQuery/13.js
@@ -0,0 +1,13 @@
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+
+    $("#target5").clone().appendTo("#left-well");
+
+
+  });
+</script>
+
