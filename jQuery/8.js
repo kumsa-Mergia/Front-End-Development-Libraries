0dd0616aadcfa033471229f949366850f2a33ace
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 10:47:40 +0300

    Change the CSS of an Element Using jQuery

---
+++ jQuery/8.js
@@ -0,0 +1,12 @@
+<script>
+  $(document).ready(function() {
+    $("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $("#target3").addClass("animated fadeOut");
+    $("button").removeClass("btn-default");
+
+    $("#target1").css("color", "red");
+
+  });
+</script>
+
