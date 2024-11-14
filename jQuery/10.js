b02e87a93fa92db4b5f9993238ab0d45273c0bba
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 10:38:20 +0300

    Change Text Inside an Element Using jQuery

---
+++ jQuery/10.js
@@ -0,0 +1,10 @@
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+
+    //Emphasize the text of #target4 by wrapping it in <em> tags
+  $("#target4").html("<em>#target4</em>");
+
+  });
+</script>
+
