bf34948e878d50848d14ebc482a9f526c45ecba8
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 10:46:08 +0300

    Target the Same Element with Multiple jQuery Selectors

---
+++ jQuery/6.js
@@ -0,0 +1,15 @@
+<script>
+  $(document).ready(function() {
+
+    // Step 1: Add the 'animated' class to all elements of type 'button'
+$("button").addClass("animated");
+
+// Step 2: Add the 'shake' class to all buttons with the class 'btn'
+$(".btn").addClass("shake");
+
+// Step 3: Add the 'btn-primary' class to the button with the id 'target1'
+$("#target1").addClass("btn-primary");
+
+  });
+</script>
+
