1e71914a0ce6a04fb7ea1d23476bd72fa6ab065c
Kumsa Mergia <kumsamergia@gmail.com>
2024-11-14 10:38:49 +0300

    Remove an Element Using jQuery

---
+++ jQuery/11.js
@@ -0,0 +1,10 @@
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+
+    // Remove the #target4 element
+  $("#target4").remove();
+  });
+</script>
+
