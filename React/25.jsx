1a9b68e022c84ef3ee648bdb7026c2a63b063628
kumsa Mergia <kumsamergia@gmail.com>
2024-12-28 07:57:13 +0300

    Render State in the User Interface Another Way

---
+++ React/25.jsx
@@ -0,0 +1,29 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        visibility: false // Initialize visibility to false
+      };
+      // Bind the toggleVisibility method to 'this'
+      this.toggleVisibility = this.toggleVisibility.bind(this);
+    }
+  
+    // Define the toggleVisibility method
+    toggleVisibility() {
+      // Use an anonymous function to toggle the visibility state
+      this.setState(prevState => ({
+        visibility: !prevState.visibility // Toggle visibility
+      }));
+    }
+  
+    render() {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          {/* Conditionally render the h1 element based on visibility state */}
+          {this.state.visibility && <h1>Now you see me!</h1>}
+        </div>
+      );
+    }
+  }
+  
